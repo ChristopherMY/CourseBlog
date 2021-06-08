ad73d4bfedf9ce2700a653eec0120370f7c363b9
Christopher Monzón <christopher_jean_pierre@outlook.com>
2021-06-08 16:57:37 -0500

    LarningLaravel

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+@section('title', 'Cursos')
+
+@section('content')
+<h1>En esta pagina podrás crear un curso.</h1>
+@endsection()
---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title')</title>
+        <!-- favicon -->
+        <!-- estilos -->
+    </head>
+    <body>
+        <!-- header -->
+        <!-- aside -->
+        @yield('content')
+
+        <!-- footer -->
+        <!-- scripts -->
+    </body>
+</html>
